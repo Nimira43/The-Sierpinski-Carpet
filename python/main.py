d33c0c8f820bf6c2e3614d84fc136aefe45da80c
Nimira43 <n.rabone72@aol.com>
2024-12-07 15:58:23 +0000

    Split the current square into nine smaller square and exclue the central one. The keep applying the process.

--- python/main.py
+++ python/main.py
@@ -4,3 +4,10 @@ import numpy as np
 def sierpinkski_carpet(x, y, size, depth):
   if depth == 0:
     plt.fill([x, x + size, x + size, x], [y, y, y + size, y + size], 'k')
+  else:
+    new_size = size / 3
+    for i in range(3):
+      for j in range(3):
+        if i == 1 and j == 1:
+          continue
+        sierpinkski_carpet(x + i * new_size, y + j * new_size, new_size, depth - 1)
