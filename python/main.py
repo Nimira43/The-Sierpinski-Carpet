24d9348ee5c0801d0064ff86edc08ea4dbc4623d
Nimira43 <n.rabone72@aol.com>
2024-12-07 15:51:50 +0000

    This will fill a black square when the recursion depth reaches 0.

--- python/main.py
+++ python/main.py
@@ -1,2 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+def sierpinkski_carpet(x, y, size, depth):
+  if depth == 0:
+    plt.fill([x, x + size, x + size, x], [y, y, y + size, y + size], 'k')
