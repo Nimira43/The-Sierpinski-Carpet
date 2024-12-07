9303925a748d17e54b8498845975a86838786c2d
Nimira43 <n.rabone72@aol.com>
2024-12-07 16:03:03 +0000

    Create new figure, call the function, set aspect ratio, turn off axis and then display the plot.

--- python/main.py
+++ python/main.py
@@ -11,3 +11,11 @@ def sierpinkski_carpet(x, y, size, depth):
         if i == 1 and j == 1:
           continue
         sierpinkski_carpet(x + i * new_size, y + j * new_size, new_size, depth - 1)
+
+plt.figure(figsize=(8, 8))
+
+# start at (0, 0) with size 1 and depth 4
+sierpinkski_carpet(0, 0, 1, 4)
+plt.axis('equal')
+plt.axis('off')
+plt.show()
