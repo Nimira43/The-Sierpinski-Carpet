7d90f31bbdb9838a30cbf32267fd235dd2ab60bb
Nimira43 <n.rabone72@aol.com>
2024-12-07 20:53:31 +0000

    Tidy up. Project completed.

--- javascript/script.js
+++ javascript/script.js
@@ -5,17 +5,17 @@ document.addEventListener('DOMContentLoaded', () => {
   const ctx = canvas.getContext('2d')
 
   function sierpinskiCarpet(x, y, size, depth) {
-      if (depth == 0) {
-          ctx.fillRect(x, y, size, size)
-      } else {
-          const newSize = size / 3
-          for (let i = 0; i < 3; i++) {
-              for (let j = 0; j < 3; j++) {
-                  if (i == 1 && j == 1) continue; // Skip the center square
-                  sierpinskiCarpet(x + i * newSize, y + j * newSize, newSize, depth - 1)
-              }
-          }
+    if (depth == 0) {
+      ctx.fillRect(x, y, size, size)
+    } else {
+      const newSize = size / 3
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          if (i == 1 && j == 1) continue; // Skip the center square
+          sierpinskiCarpet(x + i * newSize, y + j * newSize, newSize, depth - 1)
+        }
       }
+    }
   }
 
   ctx.fillStyle = 'black';
